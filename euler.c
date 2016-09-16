fecf30401a7b254b2160b02a6ed3301f750ca799
Will Ostlund <wostlund@stuy.edu>
2016-09-16 10:42:44 -0400

    made first fuction

--- euler.c
+++ euler.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
 #include< stdlib.h>
+
+int sumThreeFive(int x){
+  int i;
+  int ans = 0;
+  for(i = 0; i<x; i++){
+    if(i%3 == 0 || i%5 == 0){
+      ans += i;
+    }
+  }
+  return ans;
+}
